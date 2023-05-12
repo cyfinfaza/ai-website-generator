19bd57ae54ae0a76b3ea8a7606d79906f0995c5c
cyfinfaza <42043351+cyfinfaza@users.noreply.github.com>
2023-05-12 16:40:23 -0400

    switch to fetch and add scss

--- src/routes/api/generate/+server.js
+++ src/routes/api/generate/+server.js
@@ -3,31 +3,35 @@ import { OPENAI_API_KEY } from "$env/static/private";
 import axios from "axios";
 
 async function requestCompletion(conversation) {
-  const response = await axios.post(
-    "https://api.openai.com/v1/chat/completions",
-    {
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content:
-            "You are being used as a code generation system. The user will provide descriptions of what they want, and you will generate the code. Your code will go directly into their editor. Your responses must be in HTML format, and must start with <!DOCTYPE html>",
-        },
-        ...conversation,
-      ],
-      temperature: 0.1,
-    },
-    { headers: { Authorization: `Bearer ${OPENAI_API_KEY}` } }
-  );
-  console.log(response.data);
-  return response.data.choices[0].message;
+	const response = await fetch("https://api.openai.com/v1/chat/completions", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${OPENAI_API_KEY}`,
+		},
+		body: JSON.stringify({
+			model: "gpt-3.5-turbo",
+			messages: [
+				{
+					role: "system",
+					content:
+						"You are being used as a code generation system. The user will provide descriptions of what they want, and you will generate the code. Your code will go directly into their editor. Your responses must be in HTML format, and must start with <!DOCTYPE html>",
+				},
+				...conversation,
+			],
+			temperature: 0.1,
+		}),
+	});
+	const data = await response.json();
+	console.log(data);
+	return data.choices[0].message;
 }
 
 export async function POST({ request }) {
-  const conversation = await request.json();
-  console.log(conversation);
-  const completion = await requestCompletion(conversation);
-  return json(completion);
-  //   return text(completion.content.slice(completion.content.indexOf("<")));
-  //   return text(completion.content);
+	const conversation = await request.json();
+	console.log(conversation);
+	const completion = await requestCompletion(conversation);
+	return json(completion);
+	//   return text(completion.content.slice(completion.content.indexOf("<")));
+	//   return text(completion.content);
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-auto";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,8 +7,16 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+	},
+
+	preprocess: [
+		preprocess({
+			scss: {
+				prependData: '@use "src/variables.scss" as *;',
+			},
+		}),
+	],
 };
 
 export default config;
